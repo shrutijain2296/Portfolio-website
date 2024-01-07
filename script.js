283a95296bd3f1ee71c3941beeaa18f9bac546b6
shrutijain2296 <shrutijain2296@gmail.com>
2024-01-08 02:18:21 +0530

    changed commit

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ document.addEventListener('DOMContentLoaded', function () {
         navLinks.classList.toggle('show');
     });
 });
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycby4Lxu7PYC3-oOvlA0scXoRA1cfbJ3WZSpMO5g_sJd9gQBVud0VpB-SjRgdkuljCU1v/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg");
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML = "Message sent successfully!"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },2000);
+        form.reset();
+    })
+    .catch(error => console.error('Error!', error.message))
+})
+
+
